7098f60ca82babd88e459f84a619f3e15b47cd25
LimKyeongmuk <koungmuk1975@gmail.com>
2021-09-08 18:42:49 +0900

    main_final and module 수정(동작확인)

--- src/ar_module.py
+++ src/ar_module.py
@@ -7,22 +7,39 @@ from ar_track_alvar_msgs.msg import AlvarMarkers
 from tf.transformations import euler_from_quaternion
 
 class ArModule():
-    def __init__(self):
-        arData = {"DX":0.0, "DY":0.0, "DZ":0.0, "AX":0.0, "AY":0.0, "AZ":0.0, "AW":0.0}
-        
-    def set_arData(i):
-        arData["DX"] = i.pose.pose.position.x
-        arData["DY"] = i.pose.pose.position.y
-        arData["DZ"] = i.pose.pose.position.z
+    arData = {"DX":0.0, "DY":0.0, "DZ":0.0, "AX":0.0, "AY":0.0, "AZ":0.0, "AW":0.0}
+    i = 0
 
-        arData["AX"] = i.pose.pose.orientation.x
-        arData["AY"] = i.pose.pose.orientation.y
-        arData["AZ"] = i.pose.pose.orientation.z
-        arData["AW"] = i.pose.pose.orientation.w
-            
-    def get_id():
-        return i.id
+    def __init__(self):
+        self.arData = {"DX":0.0, "DY":0.0, "DZ":0.0, "AX":0.0, "AY":0.0, "AZ":0.0, "AW":0.0}
+        self.i = 0
+        self.roll, self.pitch, self.yaw = 0, 0, 0
+
+    def set_arData(self, data):
+        self.i = data.markers
+        print(self.i)
+
+        self.arData["DX"] = self.i.pose.pose.position.x
+        self.arData["DY"] = self.i.pose.pose.position.y
+        self.arData["DZ"] = self.i.pose.pose.position.z
+
+        self.arData["AX"] = self.i.pose.pose.orientation.x
+        self.arData["AY"] = self.i.pose.pose.orientation.y
+        self.arData["AZ"] = self.i.pose.pose.orientation.z
+        self.arData["AW"] = self.i.pose.pose.orientation.w
+        self.roll, self.pitch, self.yaw = euler_from_quaternion([self.arData["AX"], self.arData["AY"], self.arData["AZ"], self.arData["AW"]])
+
+    def get_yaw(self):
+        return self.yaw
+
+    def get_id(self):
+        return self.i.id
     
-    def get_distance():
-        return math.sqrt(pow(arData["DX"], 2) + pow(arData["DZ"], 2))
+    def get_distance(self):
+        return math.sqrt(pow(self.arData["DX"], 2) + pow(self.arData["DZ"], 2))
+
+    def is_ar(self):
+       if self.get_id() == 0 and self.get_distance() < 0.6 :
+           return True
+       return False
        
